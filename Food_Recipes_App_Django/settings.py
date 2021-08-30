7e8fc7201ab582564b6b160a915d680da0193133
shamsuchoyimadathil <shamsudheenchoyimadathil@gmail.com>
2021-08-30 14:17:23 +0530

    completed base , like created templates view,urls,css etc

--- Food_Recipes_App_Django/settings.py
+++ Food_Recipes_App_Django/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'recipe_app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Food_Recipes_App_Django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR /"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
