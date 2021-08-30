f2748a9f35422a2c971fefb97d32cb2a5596bc98
shamsuchoyimadathil <shamsudheenchoyimadathil@gmail.com>
2021-08-30 15:24:43 +0530

    created model for add recipe , and  registered in admin

--- recipe_app/admin.py
+++ recipe_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class RecipesAdmin(admin.ModelAdmin):
+    list_display = ('food_name','category',"cuisine_type")
+    list_filter = ("category","cuisine_type","native_food_of")
+
+admin.site.register(models.Recipes,RecipesAdmin)
--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+CUISINE_TYPE = (
+    ('veg','Veg'),
+    ('vegan','Vegan'),
+    ('eggetarian','Eggetarian'),
+    ('non veg','Non Veg')
+)
+
+class Recipes(models.Model):
+    food_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to = "food_images")
+    preparation_time = models.PositiveIntegerField()
+    ingredients = models.CharField(max_length=100)
+    preparation = models.TextField()
+    category = models.CharField(max_length=20)
+    native_food_of = models.CharField(blank=True,max_length=20)
+    cuisine_type = models.CharField(max_length=20,choices=CUISINE_TYPE)
+
+    def __str__(self):
+        return self.food_name
